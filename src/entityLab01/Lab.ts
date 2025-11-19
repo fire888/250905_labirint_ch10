fd45fb375691b8c0c368691766255ed794125fc2
vasilii <porokh.vs@gmail.com>
2025-11-19 10:03:26 +0300

    [LAB] process

--- src/entityLab01/Lab.ts
+++ src/entityLab01/Lab.ts
@@ -21,40 +21,58 @@ export class Labyrinth {
 
         const boxes = []
 
-        for (let i = 0; i < 1000; ++i) { 
-            const m = new THREE.Mesh(
-                new THREE.BoxGeometry(
-                    Math.random() * 10, 
-                    .3, 
-                    Math.random() * 10
-                ), 
-                mat
+        // for (let i = 0; i < 1000; ++i) { 
+        //     const m = new THREE.Mesh(
+        //         new THREE.BoxGeometry(
+        //             Math.random() * 10, 
+        //             .3, 
+        //             Math.random() * 10
+        //         ), 
+        //         mat
+        //     )
+        //     this._root.studio.add(m)
+        //     m.position.set(
+        //         Math.random() * 100 - 50, 
+        //         Math.random() * 100 - 50, 
+        //         Math.random() * 100 - 50
+        //     )
+        //     m.rotation.y = Math.random() * 2 * Math.PI
+        //     boxes.push({
+        //         n: i,
+        //         m,
+        //         pos: m.position,
+        //     })
+        //     m.userData.n = i
+        // }
+
+        const v = []
+        let currAngle = 0
+        let currentPos = [0, 0, 0]
+
+        for (let i = 0; i < 100; ++i) {
+            currAngle += (Math.random() - .5) * Math.PI * .3
+
+            const w = Math.random() * 10
+            const d = Math.random() * 5
+
+            const _v  = _M.createPolygon(
+                [-w, 0, d],
+                [w, 0, d],
+                [w, 0, -d],
+                [-w, 0, -d],
             )
-            this._root.studio.add(m)
-            m.position.set(
-                Math.random() * 100 - 50, 
-                Math.random() * 100 - 50, 
-                Math.random() * 100 - 50
-            )
-            m.rotation.y = Math.random() * 2 * Math.PI
-            boxes.push({
-                n: i,
-                m,
-                pos: m.position,
-            })
-            m.userData.n = i
+
+            _M.rotateVerticesY(_v, currAngle)
+            _M.translateVertices(_v, currentPos[0], currentPos[1], currentPos[2])
+
+            currentPos[0] += Math.sin(currAngle) * w * 2 
+            currentPos[0] += Math.cos(currAngle) * w * 2
+
+            v.push(..._v)
         }
 
-        const connectionsNearests = {}
-
-        // let savedDistdata = { b1: null, b2: null, d: Infinity }
-        // for (let i = 0; i < boxes.length; ++i) {
-        //     const b1 = boxes[i]
-        //     for (let j = i + 1; j < boxes.length; ++j) {
-        //         const b2 = boxes[j]
-        //         const d = _M.distance(b1.pos, b2.pos)
-        //         if (d < 10) {
-        //             if (!connectionsNearests[b1.n]) connectionsNearests[b1.n] = []
+        const m = _M.createMesh({ v })
+        this._root.studio.add(m)
 
     }
 }
