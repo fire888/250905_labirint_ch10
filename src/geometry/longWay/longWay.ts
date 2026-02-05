49ba8eb5aa364ca8348b1e49df2eb637daf78828
vasilii <porokh.vs@gmail.com>
2026-02-05 10:43:26 +0300

    [lab]: long way

--- src/geometry/longWay/longWay.ts
+++ src/geometry/longWay/longWay.ts
@@ -5,9 +5,8 @@ import { Root } from "index"
 import { COLOR_BLUE } from "constants/CONSTANTS"
 import * as THREE from "three" 
 import { createFloor00 } from "../floor00/floor00"
+import { createColumn01 } from "../column01/column01";
 
-// const S = 0.3
-const S = .3
 const STAIR_W = 3 
 
 enum I_TypeSeg {
@@ -27,11 +26,8 @@ type T_SEGMENT = {
     type: I_TypeSeg
 }
 
-const MIN_SEG = 20
-const MAX_SEG = 30
-const MIN_STAIR_LEN = 4
-const MAX_STAIR_LEN = 10
-
+const MIN_SEG = 5
+const MAX_SEG = 20
 
 const prepareSegments = (point0: A3, point1: A3, root: Root) => {
     const segments: T_SEGMENT[] = []
@@ -61,7 +57,7 @@ const prepareSegments = (point0: A3, point1: A3, root: Root) => {
         let newP
         let w = Math.random() * 5 + 5 
 
-        const eDist = curP.clone().setY(0).distanceTo(e)
+        const eDist = curP.clone().setY(0).distanceTo(e.clone().setY(0))
         if (eDist < MAX_SEG) {
             newDist = eDist
             newDir = e.clone().sub(curP).normalize()
@@ -74,7 +70,7 @@ const prepareSegments = (point0: A3, point1: A3, root: Root) => {
             }  else {
                 newDir = e.clone().sub(curP).setY(0).normalize().applyAxisAngle(
                     new THREE.Vector3(0, 1, 0), 
-                    (Math.random() - .5) * Math.PI * .7
+                    (Math.random() - .5) * Math.PI * .4
                 )
             }
 
@@ -83,16 +79,13 @@ const prepareSegments = (point0: A3, point1: A3, root: Root) => {
             newP = newDir.clone().multiplyScalar(newDist).add(curP)
 
             if (type === I_TypeSeg.STAIR) {
-                const newY = Math.max(1, curP.y + (Math.random() - .5) * 30)
+                const newY = Math.max(1, curP.y + (Math.random() - .5) * newDist * .8)
                 newP.setY(newY)
-                w = STAIR_W
+                w = 2 + Math.random() * STAIR_W
             }
         }
 
-        const el: T_SEGMENT = {
-            p0: curP.clone(), p1: newP, dir: newDir.clone(), w,
-            type 
-        } 
+        const el: T_SEGMENT = { p0: curP.clone(), p1: newP, dir: newDir.clone(), w, type }
 
         segments.push(el)
 
@@ -126,8 +119,21 @@ const divideStairs = (segmemtsSrc: T_SEGMENT[], root: Root): T_ROOM[] => {
             const next = segmemtsSrc[i + 1] ?? null
 
             if (prev && next) {
-                const pStairStart = prev.dir.clone().multiplyScalar(cur.w).add(cur.p0)
-                const pStairEnd = next.dir.clone().multiplyScalar(-next.w).add(cur.p1)
+                const sDir = cur.dir.clone().add(prev.dir).normalize().divideScalar(2).add(prev.dir)
+                const eDir = next.dir.clone().add(cur.dir).normalize().divideScalar(2).add(cur.dir)
+                
+                const pp0 = prev.dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5).multiplyScalar(cur.w * .5)
+                const pp1 = cur.dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5).multiplyScalar(cur.w * .5)
+                const d1 = pp0.distanceTo(pp1)
+
+                const pp2 = prev.dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5).multiplyScalar(-cur.w * .5)
+                const pp3 = cur.dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5).multiplyScalar(-cur.w * .5)
+                const d2 = pp2.distanceTo(pp3)
+
+                const d = Math.max(d1, d2)
+
+                const pStairStart = sDir.clone().multiplyScalar(d).add(cur.p0)
+                const pStairEnd = eDir.clone().multiplyScalar(-d).add(cur.p1)
 
                 const newDir = pStairEnd.clone().sub(pStairStart).setY(0).normalize()
 
@@ -138,7 +144,7 @@ const divideStairs = (segmemtsSrc: T_SEGMENT[], root: Root): T_ROOM[] => {
                     point1: pStairStart,
                     dir0: prev.dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5),
                     dir1: newDir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5),
-                    dir: pStairStart.clone().sub(cur.p0).normalize(),
+                    dir: sDir,
                     id: n,
                 } 
                 ++n
@@ -164,7 +170,7 @@ const divideStairs = (segmemtsSrc: T_SEGMENT[], root: Root): T_ROOM[] => {
                     point1: cur.p1,
                     dir0: newDir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5),
                     dir1: next.dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5),
-                    dir: cur.p1.clone().sub(pStairEnd).setY(0).normalize(),
+                    dir: eDir,
                     id: n,
                 } 
                 ++n
@@ -198,7 +204,6 @@ const divideStairs = (segmemtsSrc: T_SEGMENT[], root: Root): T_ROOM[] => {
 
 export const createLongWay = (point0: A3 = [0, 0, 0], point1: A3 = [100, 0, 0], root: Root): IArrayForBuffers => {
     const segments = prepareSegments(point0, point1, root)
-
     const segments2: T_ROOM[] = divideStairs(segments, root)
 
     const v: number[] = []
@@ -207,89 +212,23 @@ export const createLongWay = (point0: A3 = [0, 0, 0], point1: A3 = [100, 0, 0], 
     const vCollide: number[] = []
 
     segments2.forEach((s: T_ROOM, i) => {
-        const r = createFloor00(s, root)
-        v.push(...r.v)
-        c.push(...r.c)
-        uv.push(...r.uv)
-        vCollide.push(...r.vCollide)
+        { // columns
+            const center = s.point0.clone().add(s.point1.clone().sub(s.point0).multiplyScalar(.5))
+            const r0 = createColumn01(1, 1)
+            _M.translateVertices(r0.v, center.x, center.y, center.z)
+            _M.fill(r0.v, v)
+            _M.fill(r0.c, c)
+            _M.fill(r0.uv, uv)
+        }
+
+        { // floor
+            const r = createFloor00(s, root)
+            v.push(...r.v)
+            c.push(...r.c)
+            uv.push(...r.uv)
+            vCollide.push(...r.vCollide)
+        }
     })
 
     return { v, uv, c, vCollide }
 }
-
-
-
-    // // const l0 = _M.createLabel('point', [1, 0, 0], 1)
-    // // l0.position.set(...point0)
-    // // root.studio.add(l0)
-
-    // // const l1 = _M.createLabel('point_1', [1, 0, 0], 1)
-    // // l1.position.set(...point1)
-    // // root.studio.add(l1)
-
-    // // perimeter
-    // const p0: A3 = [
-    //     point0[0] + Math.cos(dir0) * w * .5,
-    //     point0[1],
-    //     point0[2] + Math.sin(dir0) * w * .5
-    // ]
-    // const p1: A3 = [
-    //     point1[0] + Math.cos(dir1) * w * .5,
-    //     point1[1],
-    //     point1[2] + Math.sin(dir1) * w * .5
-    // ]
-    // const p2: A3 = [
-    //     point1[0] - Math.cos(dir1) * w * .5,
-    //     point1[1],
-    //     point1[2] - Math.sin(dir1) * w * .5
-    // ]
-    // const p3: A3 = [
-    //     point0[0] - Math.cos(dir0) * w * .5,
-    //     point0[1],
-    //     point0[2] - Math.sin(dir0) * w * .5
-    // ]
-
-    // const v: number[] = []
-    // const c: number[] = []
-    // const uv: number[] = []
-    // const vCollide: number[] = []
-
-    // const countD = Math.ceil(d / S)
-    // const countW = Math.ceil(w / S)
-
-    // const p0_p1: THREE.Vector3[] = []
-    // const p3_p2: THREE.Vector3[] = []
-
-    // // divide front\back lines
-    // for (let i = 0; i < (countD + 1); ++i) {
-    //     const cur = i / countD
-        
-    //     p0_p1.push(new THREE.Vector3(
-    //         p0[0] * (1 - cur) + p1[0] * cur, 
-    //         p0[1], 
-    //         p0[2] * (1 - cur) + p1[2] * cur
-    //     ))
-    //     p3_p2.push(new THREE.Vector3(
-    //         p3[0] * (1 - cur) + p2[0] * cur, 
-    //         p3[1], 
-    //         p3[2] * (1 - cur) + p2[2] * cur
-    //     ))
-    // }
-
-    // // fill tiles full perimeter
-    // for (let i = 1; i < p0_p1.length; ++i) {
-    //     for (let j = 1; j < countW; ++j) {
-    //         const p0 = p0_p1[i - 1].clone().sub(p3_p2[i - 1]).multiplyScalar(j / countW).add(p3_p2[i - 1])
-    //         const p1 = p0_p1[i].clone().sub(p3_p2[i]).multiplyScalar(j / countW).add(p3_p2[i])
-    //         const p2 = p0_p1[i].clone().sub(p3_p2[i]).multiplyScalar((j - 1) / countW).add(p3_p2[i])
-    //         const p3 = p0_p1[i - 1].clone().sub(p3_p2[i - 1]).multiplyScalar((j - 1) / countW).add(p3_p2[i - 1])
-
-    //         const _v = _M.createPolygon(p0.toArray(), p1.toArray(), p2.toArray(), p3.toArray())
-    //         v.push(..._v)
-    //         c.push(..._M.fillColorFace([1, 1, 1]))
-    //         uv.push(..._M.createUv([0, 0], [1, 0], [1, 1], [0, 1]))
-    //     }
-    // }
-    
-    // const _vCollide = _M.createPolygon(p0, p1, p2, p3)
-    // vCollide.push(..._vCollide) 
